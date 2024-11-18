2a85e7b915840be6425929a201823809fdb4d30a
Aananda <atimalsina2016@gmail.com>
2024-11-18 13:19:18 +0545

    Updated paragraph text

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function changeText() {
     const paragraph = document.getElementById("text");
-    paragraph.innerText = "Just learning the development";
+
+    // Change the text content of the paragraph
+    paragraph.innerText = "Just learning the development. Also trying to learn to use Git. ";
+
+    // Change the text color
+    paragraph.style.color = "blue";  // You can change this color as desired
+
+    // Log a message to the console
+    console.log("The text has been changed!");
+
+    // Animate the paragraph by fading it in and out
+    paragraph.style.transition = "opacity 1s ease";
+    paragraph.style.opacity = 0;
+
+    setTimeout(function() {
+        paragraph.style.opacity = 1;
+    }, 1000);
 }
